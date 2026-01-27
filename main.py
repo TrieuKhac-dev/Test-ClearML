fc9d4f6c6514d1e6e2cba769e765837a44a1e408
TrieuKhac <caotrieu333@gmail.com>
2026-01-27 23:20:57 +0700

    new

--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 from dotenv import load_dotenv
 import os
 from clearml import Task
+from clearml import Task as ClearMLTask
 
 load_dotenv()
 
 # Thiết lập ClearML credentials từ .env
-os.environ["CLEARML_API_ACCESS_KEY"] = os.getenv("CLEARML_API_ACCESS_KEY")
-os.environ["CLEARML_API_SECRET_KEY"] = os.getenv("CLEARML_API_SECRET_KEY")
-os.environ["CLEARML_SERVER_HOST"] = os.getenv("CLEARML_SERVER_HOST")
+os.environ["CLEARML_API_ACCESS_KEY"] = os.getenv("CLEARML_API_ACCESS_KEY") or ""
+os.environ["CLEARML_API_SECRET_KEY"] = os.getenv("CLEARML_API_SECRET_KEY") or ""
+os.environ["CLEARML_SERVER_HOST"] = os.getenv("CLEARML_SERVER_HOST") or ""
 
 # Khởi tạo ClearML Task
-task = Task.init(
+task: ClearMLTask = Task.init(
 	project_name="test_workflows",
 	task_name="main.py run on colab via clearml"
 )
